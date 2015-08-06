fce3c474db327019a1ea947f5bdb8c823ecf9d1f
dinabseiso <dina.bseiso@ischool.berkeley.edu>
2015-08-06 12:13:36 -0700

    for merge

--- HW03_ex06.py
+++ HW03_ex06.py
@@ -7,7 +7,19 @@
 # Exercise 1
 # When you submit only include your final function: compare
 
-
+def compare(x, y):
+    if x > y :
+        temp = 1
+        print ("x is greater than y")
+        return temp
+    elif x == y : 
+        temp = 0
+        print("x and y are equal")
+        return temp
+    else : 
+        temp = -1
+        print("x is less than y")
+        return temp 
 
 
 
@@ -16,6 +28,12 @@
 # When you submit only include your final function: hypotenuse
 # Do develop incrementally. Do not share here.
 
+import math
+
+def hypotenuse(a, b):
+    cSquared = a**2 + b**2
+    c = math.sqrt(cSquared)
+    print (c)
 
 
 
@@ -24,7 +42,13 @@
 # Exercise 3
 # When you submit only include your final function: is_between
 
-
+def is_between(x, y, z):
+    if x <= y & y <=z:
+        print("True")
+        return True
+    else:
+        print("False")
+        return False
 
 
 
@@ -60,18 +84,18 @@ def main():
     ############################################################################
     # Uncomment the below to test and before commiting:
     # # Exercise 1
-    # compare(1,1)
-    # compare(1,2)
-    # compare(2,1)
+    compare(1,1)
+    compare(1,2)
+    compare(2,1)
     # # Exercise 2
-    # hypotenuse(1,1)
-    # hypotenuse(3,4)
-    # hypotenuse(1.2,12)
+    hypotenuse(1,1)
+    hypotenuse(3,4)
+    hypotenuse(1.2,12)
     # # Exercise 3
-    # is_between(1,2,3)
-    # is_between(2,1,3)
-    # is_between(3,1,2)
-    # is_between(1,1,2)
+    is_between(1,2,3)
+    is_between(2,1,3)
+    is_between(3,1,2)
+    is_between(1,1,2)
     # # Exercise 6
     # is_palindrome("Python")
     # is_palindrome("evitative")
